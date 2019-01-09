97d58203379b2e65b2289df0ca50019ffbd4e8e9
JCoDog <jayconnolly14@icloud.com>
2019-01-09 06:50:50 +0000

    Update header.php
    Added cookie consent (free cookies!!!!)

--- includes/header.php
+++ includes/header.php
@@ -96,6 +96,27 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+	<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+	<script>
+		window.addEventListener("load", function(){
+		window.cookieconsent.initialise({
+		"palette": {
+			"popup": {
+			"background": "#000000"
+			},
+			"button": {
+			"background": "#d40000"
+			}
+		},
+		"theme": "edgeless",
+		"content": {
+			"message": "To allow for you to log in to this service, we use cookies. So, this is just to tell you that you got some free cookies. Nothing more.",
+			"dismiss": "Okie Dokie",
+			"link": "Find Out More About Cookies"
+		}
+		})});
+	</script>
 </head>
 <body>
 	
