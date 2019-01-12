7dc4e3761249be55f72b5e33b55d1c740ae2851b
DAG <kaseyber12@gmail.com>
2019-01-12 12:08:41 -0800

    started admin index page
    started work on
    
    admin pannel
    settings page
    staff page
    tickets page
    users page

--- admin/includes/menu.php
+++ admin/includes/menu.php
@@ -1,20 +1,34 @@
-<nav class="navbar navbar-expand-lg fixed-top">
+<nav class="navbar navbar-expand-lg">
 		<a class="navbar-brand" href="#"><?php echo $com_name ?></a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		   MENU
-		</button>
+		
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		  	<ul class="navbar-nav mr-auto">
 		        <li class="nav-item active">
-			      	<a class="nav-link" href="login">Home <span class="sr-only">(current)</span></a>
+			      	<a class="nav-link" href="#">My Profile <span class="sr-only">(current)</span></a>
 			    </li>
-			    <li class="nav-item">
-		        	<a class="nav-link" href="register">Register</a>
-		      	</li>
-		      	<li class="nav-item">
-		        	<a class="nav-link" href="#">Documentation</a>
-		      	</li>
+			    <li class="nav-item active">
+			      	<a class="nav-link" href="#">My Support <span class="sr-only">(current)</span></a>
+			    </li>
 		    </ul>
 		</div>
+		<button class="navbar-toggler right" id="menu">
+		   MENU
+		</button>
 	</nav>
+<div class="wrapper" id="wrapper">
+	<div class="row">
+		<!-- Sidebar -->
+		<div id="sidebar-wrapper">
+			<ul class='sidebar-nav'>
+				<li><a href="index">Admin</a></li>
+				<li><a href="settings">Settings</a></li>
+				<li><a href="staff">Staff</a></li>
+				<li><a href="users">Users</a></li>
+				<li><a href="tickets">Tickets</a></li>
+				<li><a href="../dashboard">User Dashbaord</a></li>
+			</ul>
+			<a href="#" class="btn btn-danger btn-block" style="margin: 0 auto;">Log Out</a>
+		</div>
+		<!-- Page Content -->
+		
--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,64 @@
 <?php require_once "includes/header.php"; ?>
 <?php require_once "includes/menu.php"; ?>
+<div id="page-contnet-wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-4">
+						<h2 class="text-center">Manualy Validate Users</h2>
+						<?php  
+							$sql = "SELECT * FROM users WHERE user_confirmed = 0";
+							$query = query($sql);
+
+							if(rows($query) > 0) {
+								while($row = assoc($query)) {
+									$val_username = $row["user_username"];
+									$validate_user = 'validate_' . $val_username;
+									echo '
+										<div class="row" style="padding:10px; margin-top: 10px; background: #222; border-radius: 10px;">
+											<div class="col-4">
+												<p style="padding:0px;">Username: </p>
+												' . $row["user_username"] . '
+											</div>
+											<div class="col-4">
+											<p style="padding:0px;">Discord: </p>
+												' . $row["user_discord"] . '
+											</div>
+											<div class="col-4">
+												<form action="" method="post">
+													<button class="btn btn-danger btn-block" name="validate_' . $val_username . '">Validate User</button>
+												</form>
+											</div>
+										</div>	
+									';
+
+									echo $validate_user;
+									if(isset($_POST['validate_' . $val_username])) {
+										$sql = "UPDATE users SET user_confirmed = 1 WHERE user_username = '$username'";
+										query($sql);
+										header("Refresh:0");
+									}
+								}
+							} else {
+								echo "<h2 class='text-center'>All Users Have Are Confirmed And Can Login</h2>";
+							}
+						?>
+					</div>
+					<div class="col-4">
+						
+					</div>
+					<div class="col-4">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script>
+	$("#menu").click(function(e){
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+	});
+</script>
 <?php require_once "includes/footer.php"; ?>
+
